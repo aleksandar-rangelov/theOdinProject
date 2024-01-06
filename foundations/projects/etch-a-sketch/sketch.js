d660be23ed3087ac7a5cf6b28cf89f5de1fbe5b1
Aleksandar Rangelov <122702818+aleksandar-rangelov@users.noreply.github.com>
2024-01-06 23:45:31 +0200

    working etch a sketch

--- foundations/projects/etch-a-sketch/sketch.js
+++ foundations/projects/etch-a-sketch/sketch.js
@@ -1,32 +1,59 @@
 const container = document.querySelector('.container');
+const buttonNew = document.querySelector('#new');
+const buttonClear = document.querySelector('#clear');
 
-function generatePlane(number) {
-    for(let i = 0; i < number; i++) {
-        const row = document.createElement('div');     
-        row.setAttribute('class', 'row');
-        for(let j = 0; j < number; j++) {
-            const pixel = document.createElement('div');
-            pixel.setAttribute('class', 'pixel');
-            
-            row.append(pixel);
-        }
-        container.append(row);
+function generate(number) {
+    // calculate the size of each pixel
+    const size = 960 / number;
+
+    const row = document.createElement('div');
+    row.setAttribute('class', 'row');
+
+    for (let i = 0; i < number; i++) {
+        const pixel = document.createElement('div');
+        pixel.setAttribute('class', 'pixel');
+
+        pixel.style.width = `${size}px`;
+        pixel.style.height = `${size}px`;
+
+        row.append(pixel);
     }
-} 
+
+    for (let i = 0; i < number; i++) {
+        const rowClone = row.cloneNode(true);
+        container.append(rowClone);
+    }
+
+    handleHover();
+}
 
 function handleHover() {
     const pixels = document.querySelectorAll('.pixel');
-
-    pixels.forEach((pixel) => {
-        const classes = pixel.classList;
+    pixels.forEach(pixel => {
         pixel.addEventListener('mouseover', () => {
-            // if(classes.contains('colored'))
-            //     classes.remove('colored')
-            // else
-            classes.add('colored');
+            pixel.classList.add('colored');
         })
     })
 }
 
-generatePlane(16);
-handleHover();
+function removeAllPixels() {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => container.removeChild(row));
+}
+
+function clearSketch() {
+    const colored = document.querySelectorAll('.colored');
+    colored.forEach(pixel => pixel.classList.remove('colored'));
+}
+
+function randomColor() {
+    return Math.floor(Math.random() * 255);
+}
+
+buttonNew.addEventListener('click', () => {
+    removeAllPixels(container);
+    const resolution = prompt("Enter the plane size:");
+    generate(resolution);
+});
+
+buttonClear.addEventListener('click', () => clearSketch());
